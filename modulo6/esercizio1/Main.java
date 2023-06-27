8463f7ec8450475954bf2a1689dd92c5c658f02b
Anthony <anthony.cecconi.91@gmail.com>
2023-06-27 12:08:39 +0200

    roba

---
+++ modulo6/esercizio1/Main.java
@@ -0,0 +1,15 @@
+package modulo6.esercizio1;
+
+/*
+ * Esercizio 6.1 – cancArrayList Testo:
+    Realizzare una classe cancArrayList che cancelli i numeri pari presenti in un ArrayList di interi. Tale
+    classe presenta una sola variabile di istanza private ArrayList<Integer> lista, creare quindi un
+    metodo void inserisci(int n) che inserisca nell’ArrayList lista il numero intero passato come
+    parametro esplicito, creare un metodo void cancLista() che cancelli dall’ArrayList lista tutti gli interi
+    pari, implementare infine un metodo String stampa che restituisce sottoforma di stringa il
+    contenuto dell’ ArrayList lista. 
+ */
+
+public class Main {
+    
+}
---
+++ modulo6/esercizio1/classi/CancArrayList.java
@@ -0,0 +1,12 @@
+package modulo6.esercizio1.classi;
+
+import java.util.ArrayList;
+
+public class CancArrayList {
+
+    public ArrayList cancellEven(ArrayList list){
+
+        return new ArrayList<>();
+    }
+    
+}
